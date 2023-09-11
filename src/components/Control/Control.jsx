9e84fdc3aee4b2fe481a4f4fc1367a41136fedf8
Dudkin Sergey <dsergik@mail.ru>
2023-09-11 21:47:54 +0300

    добавил  html  код

--- src/components/Control/Control.jsx
+++ src/components/Control/Control.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './control.scss';
+import control from '../../img/images/section-5/control_svg.svg'
+
 
 const Control = () => {
   return (
-    <section className='control'>
-        <div className='container'>
-        Control
-
+    <section className="control">
+      <div className="container">
+        <div className="control__content">
+          <h2 className="control__title title">
+            С ИНДИВИДУАЛЬНЫМИ НАСТРОЙКАМИ
+          </h2>
+          <div className="control__info-text">
+            <p>
+              Мышь MX Master 3 адаптируется под ваши рабочие процессы. Благодаря
+              индивидуальным настройкам, вы сможете настроить кнопки и жесты
+              практически под любое используемое приложение.
+            </p>
+          </div>
+          <img src={control} alt="" />
         </div>
-        </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
 export default Control;
